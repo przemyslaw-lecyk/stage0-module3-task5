a25c08921b6d3b7fd34666fa35d39f3e6f4014fb
Przemysław Lecyk <przemyslaw@lecyk.net>
2022-10-25 23:46:13 +0200

    Operators

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,14 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
+    public static void main(String[] args) {
+        //example
+        NumberDivider numberDivider = new NumberDivider();
+        numberDivider.divide(19);
+
+    }
+
     void divide(int toBeDivided) {
+        System.out.println(toBeDivided / 5.0);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
+    public static void main(String[] args) {
+        //example
+        TimeConvertor convertor = new TimeConvertor();
+        convertor.convert(11.5F);
+    }
     public void convert(float minutes) {
+        System.out.println(minutes * 60.0);
     }
 }
