db7fa5821cd14339f9c96aa930f315b0d4b4b8f3
Przemysław Lecyk <przemyslaw@lecyk.net>
2022-10-25 23:59:07 +0200

    Operators again

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,14 +1,8 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    public static void main(String[] args) {
-        //example
-        NumberDivider numberDivider = new NumberDivider();
-        numberDivider.divide(19);
-
-    }
 
     void divide(int toBeDivided) {
-        System.out.println(toBeDivided / 5.0);
+        toBeDivided /= 5.0;
     }
 }
