16161075e954afe80cde072ee77f82e94d119bf4
Przemysław Lecyk <przemyslaw@lecyk.net>
2022-10-25 23:56:47 +0200

    Operators another try

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,12 +1,8 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
-    public static void main(String[] args) {
-        //example
-        TimeConvertor convertor = new TimeConvertor();
-        convertor.convert(11.5F);
-    }
+
     public void convert(float minutes) {
-        System.out.println(minutes * 60.0);
+        minutes *= 60.0;
     }
 }
